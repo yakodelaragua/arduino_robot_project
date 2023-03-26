0a81f149753d7adad72b6823006c9920707919bc
yakodelaragua <araycdd@gmail.com>
2023-03-26 15:42:15 +0200

    añadido seguimiento de luz

--- seguimiento_luz/seguimiento_luz.ino
+++ seguimiento_luz/seguimiento_luz.ino
@@ -1,6 +1,7 @@
 //Photoresistor
-#define light_Pin_L A1
-#define light_Pin_R A2
+#define light_Pin_L A2
+#define light_Pin_R A1
+
 //Motor
 #define motor_Direc_L 4
 #define motor_PWM_L 6
@@ -9,6 +10,9 @@
 
 int left_light;
 int right_light;
+
+int motor_PWM_LOW = 225;
+int motor_PWM_HIGH = 55;
 
 void setup() {
   Serial.begin(9600);
@@ -30,6 +34,53 @@ void loop() {
   Serial.println(left_light);
   Serial.print("Valor luz derecha: ");
   Serial.println(right_light);
+  
 
+  if(left_light > 650 && right_light > 650) { //Recto
+    Tank_front();
+    
+  } else if(left_light <= 650 && right_light > 650) { //Izquierda
+    Tank_left();
+
+  } else if(left_light > 650 && right_light <= 650) { //Derecha
+    Tank_right();
+
+  } else {  //Para
+    Tank_stop();
+    
+  }
+  
+  
+}
+
+void Tank_front() {
+  digitalWrite(motor_Direc_L, HIGH);
+  analogWrite(motor_PWM_L, motor_PWM_HIGH);
+  digitalWrite(motor_Direc_R, HIGH);
+  analogWrite(motor_PWM_R, motor_PWM_HIGH);
+  
+}
+
+void Tank_left() {
+  digitalWrite(motor_Direc_L, LOW);
+  analogWrite(motor_PWM_L, motor_PWM_LOW);
+  digitalWrite(motor_Direc_R, HIGH);
+  analogWrite(motor_PWM_R, motor_PWM_HIGH);
+  
+}
+
+void Tank_right() {
+  digitalWrite(motor_Direc_L, HIGH);
+  analogWrite(motor_PWM_L, motor_PWM_HIGH);
+  digitalWrite(motor_Direc_R, LOW);
+  analogWrite(motor_PWM_R, motor_PWM_LOW);
+  
+}
+
+void Tank_stop() {
+  digitalWrite(motor_Direc_L, LOW);
+  analogWrite(motor_PWM_L, 0);
+  digitalWrite(motor_Direc_R, LOW);
+  analogWrite(motor_PWM_R, 0);
   
 }
